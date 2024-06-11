625c1287727e853b76915edb69007e326ebcb793
gui-agt <gui.agt34@gmail.com>
2024-06-11 12:53:58 +0200

    git dots adding

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,35 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+// left arrow select and click event
+const arrow_left = document.querySelector(".arrow_left")
+arrow_left.addEventListener('click', arrow_left_click)
+// alert for left arrow click
+function arrow_left_click () {alert ('bonjour gauche')}
+
+// right arrow select and click event
+const arrow_right = document.querySelector(".arrow_right")
+arrow_right.addEventListener('click', arrow_right_click)
+// alert for right arrow click
+function arrow_right_click () {alert ('bonjour droite')}
+
+// display dots
+const dots = document.querySelector(".dots");
+let dotsIndex = 0;
+
+const displayDots = () => {
+	dots.innerHTML = ""; // empty all elements
+	for (let i = 0; i < slides.length; i++) {
+	  const div = document.createElement("div");
+	  div.classList.add("dot");
+	  dots.appendChild(div);
+	  if (i === dotsIndex) {
+		div.classList.add("dot_selected");
+	  }
+	}
+  };
+
+  displayDots();
